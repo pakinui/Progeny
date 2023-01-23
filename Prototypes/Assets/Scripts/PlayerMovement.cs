80edefcd015de1f2c7ec773aec6db7b6a5c54f03
Oliver Kenning <kenol888@student.otago.ac.nz>
2023-01-23 16:28:29 +1300

    player movement added

---
+++ Prototypes/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+    private float direction;
+    private bool facingRight = true;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Input.GetAxis("Horizontal");
+
+        if (direction > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (direction < 0 && facingRight)
+        {
+            Flip();
+        }
+        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
