9ddebc61408145193f1e9f55f4446741d54db052
rqian <qiaru110@student.otago.ac.nz>
2023-01-25 22:39:18 +0800

    optimize the jump and add the shooting

---
+++ Prototypes/Assets/Scripts/Ruiyi/TriggerCheck2D.cs
@@ -0,0 +1,36 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+
+
+public class TriggerCheck2D : MonoBehaviour
+{
+    public LayerMask mLayerMask;
+    public int triggerCount;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (CheckLayerMask(other.GameObject()))
+        {
+            triggerCount++;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (CheckLayerMask(other.GameObject()))
+        {
+            triggerCount--;
+        }
+    }
+
+    private bool CheckLayerMask(GameObject other)
+    {
+        return mLayerMask == (mLayerMask | (1 << other.layer));
+    }
+    
+    public bool Triggered()
+    {
+        return triggerCount > 0;
+    }
+}
