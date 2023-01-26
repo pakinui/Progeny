1a1eab887259faa974cfbd85067d90fdfaac58a1
schpo592 <schpo592@student.otago.ac.nz>
2023-01-26 17:25:05 +1300

    cutscene prototype working

--- Prototypes/Assets/Scripts/PlayerMovement.cs
+++ Prototypes/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
 
-    private Rigidbody2D rb;
+    //public so cutscenes prototype can access it
+    public Rigidbody2D rb;
     private float direction;
     private bool facingRight = true;
     public AudioClip footsteps;
     AudioSource audioSource;
+
+    //to stop player from being able to move during cutscenes.
+    public static bool moving = true;
 
     void Awake()
     {
@@ -19,35 +23,43 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       audioSource = GetComponent<AudioSource>();
+       //audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        direction = Input.GetAxis("Horizontal");
+        if(moving){
+            direction = Input.GetAxis("Horizontal");
 
-        if (direction > 0 && !facingRight)
-        {
-            Flip();
-        }
-        else if (direction < 0 && facingRight)
-        {
-            Flip();
-        }
-        Vector2 velocity = new Vector2(direction * speed, rb.velocity.y);
-        rb.velocity = velocity;
-        if (direction != 0 && !audioSource.isPlaying)
-        {
-            audioSource.PlayOneShot(footsteps, 1f);
-        }
+            if (direction > 0 && !facingRight)
+            {
+                Flip();
+            }
+            else if (direction < 0 && facingRight)
+            {
+                Flip();
+            }
+            Vector2 velocity = new Vector2(direction * speed, rb.velocity.y);
+            rb.velocity = velocity;
+            // if (direction != 0 && !audioSource.isPlaying)
+            // {
+            //     audioSource.PlayOneShot(footsteps, 1f);
+            // }
 
-        // TODO : jump
+            // TODO : jump
+        }
+        
     }
 
     private void Flip()
     {
         facingRight = !facingRight;
         transform.Rotate(0f, 180f, 0f);
+    }
+
+
+    public void setAbilityToMove(bool b){
+        moving = b;
     }
 }
