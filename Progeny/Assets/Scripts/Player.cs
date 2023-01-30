dfaf9deb45eb2f669ad32e95bd895a4288408659
seaja052 <seaja052@student.otago.ac.nz>
2023-01-30 17:49:28 +1300

    split Player script

--- Progeny/Assets/Scripts/Player.cs
+++ Progeny/Assets/Scripts/Player.cs
@@ -5,77 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // player states
-    private bool isMoving, isCrouching, isClimbing, isShooting, isReloading = false;
-
-    // movement speed multiplier
-    // will increase/decrease depending on player state
-    public float movementSpeed = 2f;
+    public bool isMoving, isCrouching, isClimbing, isAiming, isShooting, isReloading = false;
     // direction of player.
-    private bool facingRight = true;
-    // reference to the RigidBody component
-    private Rigidbody2D rb;
-    // reference to the main camera object
-    private Camera mainCam;
-    // position of the mouse
-    private Vector3 mousePos;
-    // reference to the bullet object
-    public GameObject bullet;
-    // reference to the bullet spawn point (end of barrel)
-    public Transform bulletSpawnPoint;
-
-    private bool canShoot = true;
-    private float timeSinceLastShot;
-    public float cooldown;
+    public bool facingRight = true;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-    }
+    static void Start(){}
 
     // Update is called once per frame
-    void Update()
-    {
-        // update mouse position
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition); 
-        // calculate suitable rotation
-        Vector3 rotation = mousePos - transform.position;
-        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        // flip Player object if suitable
-        if(Mathf.Abs(rotZ) > 90 && facingRight)
-        {
-            Flip();
-        }
-        else if(Mathf.Abs(rotZ) < 90 && !facingRight)
-        {
-            Flip();
-        }
-        // rotate gun
-        transform.GetChild(0).rotation = Quaternion.Euler(0,0,rotZ);
-
-        // horizontal movement input
-        float horizontalAxis = Input.GetAxis("Horizontal");
-
-        // horizontal movement
-        if(!isClimbing && horizontalAxis != 0)
-        {
-            isMoving = true;
-            rb.velocity = new Vector2(horizontalAxis * movementSpeed, rb.velocity.y);
-        }
-        else
-        {
-            isMoving = false;
-        }
-
-    }
+    static void Update(){}
 
     // method to flip the Player object and update the direction variable
-    void Flip()
+    public void Flip()
     {
         facingRight = !facingRight;
         transform.Rotate(0f, 180f, 0f);
-        transform.GetChild(0).Rotate(0f, 0f, 180f);
     }
 
 }
