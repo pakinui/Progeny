5d8f31d86b27dad1b8bbcae9613f4305c7740b2c
schpo592 <schpo592@student.otago.ac.nz>
2023-01-30 16:39:01 +1300

    Adding backgrounds and parallax to level 1.

---
+++ Progeny/Assets/Scripts/Parallax.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+    Parallax makes the background move is comparison to
+    how far away it is from the player.
+
+*/
+public class Parallax : MonoBehaviour
+{
+    public Camera cam;
+
+    public Transform player;
+
+    Vector2 startPosition;
+
+    float startZ;
+
+    Vector2 travel => (Vector2)cam.transform.position - startPosition;
+
+    float distanceFromPlayer => transform.position.z - player.position.z;
+
+    float clippingPlane => (cam.transform.position.z + (distanceFromPlayer > 0 ? cam.farClipPlane : cam.nearClipPlane));
+    
+    float parallaxFactor => Mathf.Abs(distanceFromPlayer) / clippingPlane;
+
+    public void Start(){
+        startPosition = transform.position;
+        startZ = transform.position.z;
+    }
+
+    public void Update(){
+        Vector2 newPos = startPosition + travel * parallaxFactor;
+        transform.position = new Vector3(newPos.x, newPos.y, startZ);
+    }
+}
+
