f662dfbe5de8dc530e0b73e67c96db347ece0991
seaja052 <seaja052@student.otago.ac.nz>
2023-01-31 12:48:49 +1300

    tweaks to CameraController

--- Progeny/Assets/Scripts/CameraController.cs
+++ Progeny/Assets/Scripts/CameraController.cs
@@ -4,32 +4,42 @@ namespace Ruiyi.Controller.GameController
 {
     public class CameraController : MonoBehaviour
     {
+        // reference to the player
+        public Transform player;
+        // reference to the position of the player
+        private Vector3 playerPos;
+        // reference to the targetted position for the camera
+        private Vector3 targetPos;
+        
+
+        // speed at which the camera follows the player
         public float smoothSpeed = 4f;
+        // offset of camera position
         public float xOffset = 3f;
         public float yOffset = 3.25f;
-        
-        public Transform player;
 
         // private float xMin = -5;
         // private float xMax = 5;
         // private float yMin = -5;
         // private float yMax = 5;
 
-        private Vector3 targetPos;
-        private Vector3 playerPos;
-
+        // Start is called before the first frame update
         void Start()
         {
+            // assigning references
             player = GameObject.Find("Player").transform;
             playerPos = player.position;
             targetPos = new Vector3(playerPos.x + xOffset, playerPos.y + yOffset, -10f);
         }
 
-        void LateUpdate()
+        // FixedUpdate is called once per fixed frame-rate frame
+        void FixedUpdate()
         {
+            // direction determined variable
             float isRight = Mathf.Sign(player.localScale.x);
 
-            Vector3 playerPos = player.position;
+            // reassigning references
+            playerPos = player.position;
             targetPos.x = (playerPos.x + xOffset) * isRight;
             targetPos.y = playerPos.y + yOffset;
 
