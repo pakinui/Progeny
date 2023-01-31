f918088abbc7ceb7405d47ef68736a3ec0736382
kenol888 <kenol888@student.otago.ac.nz>
2023-01-31 15:15:36 +1300

    placeholder e symbol now appears

--- Progeny/Assets/Scripts/Interactable/Door.cs
+++ Progeny/Assets/Scripts/Interactable/Door.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    public GameObject displayPrefab;
+    private GameObject display;
     private bool isUsable;
     // Start is called before the first frame update
     void Start()
@@ -14,17 +16,19 @@ public class Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("e") && isUsable){
+        if (isUsable && Input.GetKeyDown("e")){
             Destroy(this.gameObject);
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
         isUsable = true;
+        display = Instantiate(displayPrefab, this.transform, true);
     }
 
     void OnCollisionExit2D(Collision2D collision) {
         isUsable = false;
+        Destroy(display);
     }
 
 }
