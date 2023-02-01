fba68d25ca889e69aba48b00689a73139048f4df
rqian <qiaru110@student.otago.ac.nz>
2023-02-02 00:51:49 +0800

    Add main menu screen, setting menu and pause screen

---
+++ Progeny/Assets/Scripts/Controller/GamePanelController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Controller
+{
+    public class GamePanelController : MonoBehaviour
+    {
+        public GameObject pausePanel;
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("Quit Game");
+                Application.Quit();
+            }
+
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                Debug.Log("Pause Game");
+                pausePanel.SetActive(true);
+                //gameObject.SetActive(false);
+            }
+        }
+    }
+}
