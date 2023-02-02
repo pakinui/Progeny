17b68ad710885445898404c6b6b15db7c0e09fdf
schpo592 <schpo592@student.otago.ac.nz>
2023-02-02 20:00:18 +1300

    ThoughtBubble and Story components, not complete

--- Progeny/Assets/Scripts/Interactable/TutorialTiles.cs
+++ Progeny/Assets/Scripts/Interactable/TutorialTiles.cs
@@ -12,23 +12,39 @@ public class TutorialTiles : MonoBehaviour
     */
     public int targetTutorial;
 
+    //public PounceEnemy enemy;
 
+    public TextAsset textFile;
 
     //private booleans to check if tutorials have been activated
     //or is it better to just delete the obj after the tutorial is complete??
     private bool[] tutorialsCompleted = new bool[3]{false, false, false};
     
 
+    
+
+     private Player player;
+     private StoryText story;
+     private PounceEnemy enemy;
+     // private StoryText story;
+    private ThoughtBubble bubble;
+
+    void Start(){
+         player = GameObject.Find("Player").GetComponent<Player>();
+         enemy = GameObject.Find("ShootingEnemy"). GetComponent<PounceEnemy>();
+        story = gameObject.GetComponent<StoryText>();
+        bubble =  GameObject.FindWithTag("ThoughtBubble").GetComponent<ThoughtBubble>();
+        // textFile = GetComponent<TextAsset>();
+    }
 
     void OnTriggerEnter2D(Collider2D coll){
         if(coll.tag == "Player"){
             //trigger tutorial
             if(targetTutorial == 0 && !tutorialsCompleted[0]){
-                ShootingTutorial();
-            }else if(targetTutorial == 1 && !tutorialsCompleted[1]){
-                ClimbingTutorial();
-            }else if(targetTutorial == 2 && !tutorialsCompleted[2]){
-                CrouchingTutorial();
+                stopPlayerMovement();
+                story.PlayStoryText(textFile);
+                bubble =  GameObject.FindWithTag("ThoughtBubble").GetComponent<ThoughtBubble>();
+
             }
 
             //else tutorial has been done already
@@ -38,29 +54,42 @@ public class TutorialTiles : MonoBehaviour
 
 
 
+    void Update(){
 
+        if(story.storyComplete && !tutorialsCompleted[0]){
+
+            bubble.SetBubbleText("story.theText.text");
+            //story.textBox.SetActive(false);
+            bubble.ShowBubble();
+            
+        }
+
+        if(enemy.health == 0 && !tutorialsCompleted[0]){
+            Debug.Log("died");
+            //story.textBox.SetActive(false);
+            bubble.hideBubble();
+            tutorialsCompleted[0] = true;
+        }
+    }
 
 
 
     void ShootingTutorial(){
-        Debug.Log("shooting tutorial");
-        Time.timeScale = 0;
-
-
-        tutorialsCompleted[0] = true;
+        
+        
     }
 
-    void ClimbingTutorial(){
+    
 
 
-
-        tutorialsCompleted[1] = true;
+    void stopPlayerMovement(){
+        player.setAllowedMovement(false);
+        player.setMoving(false);
     }
 
-    void CrouchingTutorial(){
-
-
-
-        tutorialsCompleted[2] = true;
+    
+    void startPlayerMovement(){
+        player.setAllowedMovement(true);
+        player.setMoving(true);
     }
 }
