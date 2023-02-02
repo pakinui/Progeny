fcab318d113b64567982c37b146b8bb8f044d6b2
kenol888 <kenol888@student.otago.ac.nz>
2023-02-02 20:23:47 +1300

    flying enemy added

---
+++ Progeny/Assets/Scripts/Enemies/FlyingEnemyAttack.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyAttack : MonoBehaviour
+{
+    // reference to the player
+    Player player;
+    // damage dealt to player on successful atttack (currently does a third)
+    public float attackDamage = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update(){}
+
+    // attack
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {    
+            float newHealth;
+            player.SetCurrentHealth(newHealth = player.GetCurrentHealth() - attackDamage);
+            Debug.Log("OUCH! health: " + newHealth);
+            if (newHealth <= 0) {player.Die();}
+
+            player.setRed(true);
+        }
+    }
+}
