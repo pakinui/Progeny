0a4e42c55550a8bdef8b9d3dc50eab41e690134c
schpo592 <schpo592@student.otago.ac.nz>
2023-02-02 16:28:40 +1300

    working on tutorial tiles

--- Progeny/Assets/Scripts/Interactable/TutorialTiles.cs
+++ Progeny/Assets/Scripts/Interactable/TutorialTiles.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class TutorialTiles : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    
+    /**
+        0 = shooting tut
+        1 = climbing tut
+        2 = pouncing/crouching tut
+    */
+    public int targetTutorial;
+
+
+
+    //private booleans to check if tutorials have been activated
+    //or is it better to just delete the obj after the tutorial is complete??
+    private bool[] tutorialsCompleted = new bool[3]{false, false, false};
+    
+
+
+    void OnTriggerEnter2D(Collider2D coll){
+        if(coll.tag == "Player"){
+            //trigger tutorial
+            if(targetTutorial == 0 && !tutorialsCompleted[0]){
+                ShootingTutorial();
+            }else if(targetTutorial == 1 && !tutorialsCompleted[1]){
+                ClimbingTutorial();
+            }else if(targetTutorial == 2 && !tutorialsCompleted[2]){
+                CrouchingTutorial();
+            }
+
+            //else tutorial has been done already
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+
+
+
+
+
+    void ShootingTutorial(){
+        Debug.Log("shooting tutorial");
+        Time.timeScale = 0;
+
+
+        tutorialsCompleted[0] = true;
+    }
+
+    void ClimbingTutorial(){
+
+
+
+        tutorialsCompleted[1] = true;
+    }
+
+    void CrouchingTutorial(){
+
+
+
+        tutorialsCompleted[2] = true;
     }
 }
