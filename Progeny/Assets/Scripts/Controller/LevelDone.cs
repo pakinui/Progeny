a02c124e07a708e5ddf55b14fb6f2403222e17d9
schpo592 <schpo592@student.otago.ac.nz>
2023-02-06 12:34:14 +1300

    added level done scene and connected it to level1

---
+++ Progeny/Assets/Scripts/Controller/LevelDone.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelDone : MonoBehaviour
+{
+    
+
+
+
+    public void BackToMenu(){
+
+        SceneManager.LoadScene("MainMenu");
+    }
+}
