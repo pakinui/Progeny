53955cce24cd624c54ce27b351cf4f5956de42ad
seaja052 <seaja052@student.otago.ac.nz>
2023-02-08 20:55:19 +1300

    nest range and range-toggle

--- Progeny/Assets/Scripts/Enemies/CritterNest.cs
+++ Progeny/Assets/Scripts/Enemies/CritterNest.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class CritterNest : MonoBehaviour
 {
+    public Transform player; // reference to the player (for distance)
     public GameObject critter; // reference to the critter object to be spawned
     public int health; // shots taken to destroy nest
     public float spawnFrequency; // rate at which critters are spawned
     private float spawnTimer; // keeps track of frequency between spawns
     // public int spawnQuantity = 1; // quantity of critters to be spawned each spawn
     // public int spawnLimit; // limit to how many critters can be spawned
+    public bool rangeEnabled;
+    public float spawnRange; // range in which the player has to be in order to spawn a critter
     
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player").transform;
         spawnTimer = spawnFrequency;
     }
 
@@ -22,14 +26,17 @@ public class CritterNest : MonoBehaviour
     void Update()
     {
         spawnTimer -= Time.deltaTime;
-        if(spawnTimer <= 0)
+        if(Mathf.Abs(player.position.x - transform.position.x) <= spawnRange || !rangeEnabled)
         {
-            //for(int i = 0; i < spawnQuantity; i++){
-            Transform spawn = Instantiate(critter).transform;
-            spawn.position = transform.position;
-            spawnTimer = spawnFrequency;
-            //}
-            // spawnLimit--;
+            if(spawnTimer <= 0)
+            {
+                //for(int i = 0; i < spawnQuantity; i++){
+                Transform spawn = Instantiate(critter).transform;
+                spawn.position = transform.position;
+                spawnTimer = spawnFrequency;
+                //}
+                // spawnLimit--;
+            }
         }
     }
 
