a6504f74a5ab0e2c68d2a4b0f7630236324b8278
schpo592 <schpo592@student.otago.ac.nz>
2023-02-11 19:34:31 +1300

    pounce animation working, dash, approach and death not yet

--- Progeny/Assets/Scripts/Animation/StandardEnemyController.cs
+++ Progeny/Assets/Scripts/Animation/StandardEnemyController.cs
@@ -5,8 +5,15 @@ using System.Collections;
 public class StandardEnemyController : MonoBehaviour {
 
    // An array with the sprites used for animation
-   public Sprite[] animSprites;
    
+   public Sprite[] idleSprites;
+   public Sprite[] approachSprites;
+   public Sprite[] pouncePrepSprites;
+   public Sprite[] pounceSprites;
+   public Sprite[] deathSprites;
+   
+   private Sprite[] currSprites;//current sprites playing
+
    // Controls how fast to change the sprites when
    // animation is running
    public float framesPerSecond;
@@ -15,35 +22,40 @@ public class StandardEnemyController : MonoBehaviour {
    // game object
    SpriteRenderer animRenderer;
    
-   // Time passed since the start of animatin
+   // Time passed since the start of animation
    private float timeAtAnimStart;
    
    // Indicates whether animation is running or not
    private bool animRunning = false;
+
+   private GroundEnemy ground;
+
+   private bool deathAnimation = false;//if currently doing death animation
+
+   //stuff to check if last frame is apart of the same animation
+   private bool idle = false;
+   private bool approach = false;
+   private bool pouncePrep = false;
+   private bool pounce = false;
+   private bool death = false;
    
    // Use this for initialization
    void Start () {
       // Get a reference to game object renderer and
       // cast it to a Sprite Rendere
       animRenderer = GetComponent<Renderer>() as SpriteRenderer;
+      ground = GetComponent<GroundEnemy>();
+
+      // Animation will start playing
+      animRunning = true;
+            
+      // Record time at animation start
+      timeAtAnimStart = Time.timeSinceLevelLoad;
+
+     
    }
 
-   // At fixed time intervals...
-   void FixedUpdate () {
-      if(!animRunning) {
-         // The animation is triggered by user input
-         float userInput = Input.GetAxis("Horizontal");
-         if(userInput != 0f) {
-            // User pressed the move left or right button
-            
-            // Animation will start playing
-            animRunning = true;
-            
-            // Record time at animation start
-            timeAtAnimStart = Time.timeSinceLevelLoad;
-         }
-      }
-   }
+   
 
    // Before rendering next frame...
    void Update () {
@@ -52,6 +64,39 @@ public class StandardEnemyController : MonoBehaviour {
          // Animation is running, so we need to 
          // figure out what frame to use at this point
          // in time
+
+         if(ground.state == GroundEnemy.State.Idle && !idle){
+            
+               currSprites = idleSprites;
+               idle = true;
+               timeAtAnimStart = Time.timeSinceLevelLoad;
+            
+            
+            //frameIndex = 0;
+         }else if(ground.state == GroundEnemy.State.Approach   || ground.state == GroundEnemy.State.Dash  || ground.state == GroundEnemy.State.DashPrep){
+            if(!approach){
+               currSprites = approachSprites;
+            approach = true;
+            idle = false;
+            pouncePrep = false;
+            timeAtAnimStart = Time.timeSinceLevelLoad;
+            }
+            
+            //frameIndex = 0;
+         }else if(ground.state == GroundEnemy.State.PouncePrep && !pouncePrep){
+            currSprites = pounceSprites;
+            pouncePrep = true;
+            approach = false;
+            timeAtAnimStart = Time.timeSinceLevelLoad;
+            //frameIndex = 0;
+         }
+         // }else if(ground.state == GroundEnemy.State.Pounce && !pounce){
+         //    currSprites = pounceSprites;
+         //    pounce = true;
+         //    pouncePrep = false;
+         //    timeAtAnimStart = Time.timeSinceLevelLoad;
+         //    //frameIndex = 0;
+         // }
          
          // Compute number of seconds since animation started playing
          float timeSinceAnimStart = Time.timeSinceLevelLoad - timeAtAnimStart;
@@ -59,16 +104,16 @@ public class StandardEnemyController : MonoBehaviour {
          // Compute the index of the next frame    
          int frameIndex = (int) (timeSinceAnimStart * framesPerSecond);
          
-         if(frameIndex < animSprites.Length) {
+         if(frameIndex < currSprites.Length) {
             // Let the renderer know which sprite to
             // use next      
-            animRenderer.sprite = animSprites[ frameIndex ];
+            animRenderer.sprite = currSprites[ frameIndex ];
          } else {
             // Animation finished, set the render
             // with the first sprite and stop the 
             // animation
-            animRenderer.sprite = animSprites[0];
-            animRunning = false;
+            animRenderer.sprite = currSprites[0];
+            // animRunning = false;
          }
       }   
    }
