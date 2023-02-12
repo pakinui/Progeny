a8958bd9d08a036eab868a32c9fae964438559d2
rqian <qiaru110@student.otago.ac.nz>
2023-02-13 12:34:49 +1300

    fix ammo count error

--- Progeny/Assets/Scripts/Controller/DisceateBarController.cs
+++ Progeny/Assets/Scripts/Controller/DisceateBarController.cs
@@ -8,20 +8,19 @@ public class DisceateBarController : MonoBehaviour
     public GameObject toggle;
     public Sprite fullHeart;
     public Sprite emptyHeart;
-    
+
     // Start is called before the first frame update
     private Player player;
     private Gun gun;
     private int health = 0;
     private int maxHealth;
-    private Image [] healthBlocks;
-    private Image [] healthTogs;
+    private Image[] healthBlocks;
+    private Image[] healthTogs;
     private bool holdGun = false;
-    
+
     private void Start()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
-        
         toggle.SetActive(false);
     }
 
@@ -32,28 +31,28 @@ public class DisceateBarController : MonoBehaviour
         healthBlocks = new Image[maxHealth];
         for (var i = 0; i < maxHealth; i++)
         {
-            
+
             var block = Instantiate(toggle, transform);
             block.transform.localPosition -= new Vector3(25 * i, 0, 0);
             block.SetActive(true);
-            
+
             healthBlocks[i] = block.GetComponent<Image>();
-            
+
             var tog = Instantiate(toggle, transform);
             tog.transform.localPosition -= new Vector3(25 * i, 0, 0);
             tog.SetActive(false);
             healthTogs[i] = tog.GetComponent<Image>();
             healthTogs[i].sprite = fullHeart;
             healthTogs[i].rectTransform.sizeDelta = fullHeart.rect.size;
-                        
+
         }
     }
-    
+
     private float getHealth()
     {
         return gun.ammoLeft * 1.0f / gun.ammoCapacity;
     }
-    
+
     // Update is called once per frame
     private void Update()
     {
@@ -66,8 +65,15 @@ public class DisceateBarController : MonoBehaviour
         else if (player.gun == null && holdGun)
         {
             holdGun = false;
+            for (var i = 0; i < maxHealth; i++)
+            {
+                Destroy(healthBlocks[i].gameObject);
+                Destroy(healthTogs[i].gameObject);
+            }
         }
-        
+
+        if (!holdGun) return;
+
         // ceil rounds up
         var targetHealth = Mathf.CeilToInt(getHealth() * maxHealth);
         if (targetHealth < health)
