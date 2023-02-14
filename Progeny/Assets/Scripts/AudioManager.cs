8636a619a54a625d680b84cd1e4fd8e6869b98a3
rqian <qiaru110@student.otago.ac.nz>
2023-02-15 02:57:51 +1300

    enable volume slider

---
+++ Progeny/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider Slider1;
+    public Slider musicSlider;
+    public Slider Slider3;
+
+    private void OnEnable()
+    {
+        audioMixer.SetFloat("VolumeOfMusic", PlayerPrefs.GetFloat("VolumeOfMusic", 0));
+    }
+    public void SetMasterVolume()
+    {
+        //audioMixer.SetFloat( "VolumeOfMusic", Slider1.value );
+    }
+
+    public void SetMusicVolume()
+    {
+        audioMixer.SetFloat("VolumeOfMusic", musicSlider.value);
+    }
+
+    public void SetMusic2Volume()
+    {
+        //audioMixer.SetFloat( "Music2Param", Slider3.value );
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat("VolumeOfMusic", musicSlider.value);
+    }
+}
