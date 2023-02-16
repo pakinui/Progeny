0376e6279d89d123bfcc1e5cf8bc01fac57588a2
kenol888 <kenol888@student.otago.ac.nz>
2023-02-16 13:15:14 +1300

    routing prefabs to effect mixer and adding death sounds

--- Progeny/Assets/Scripts/Animation/EnemyDeathAnimator.cs
+++ Progeny/Assets/Scripts/Animation/EnemyDeathAnimator.cs
@@ -8,13 +8,14 @@ public class EnemyDeathAnimator : MonoBehaviour
     private SpriteRenderer animRenderer;
     public float framesPerSecond;
     private float timeAtAnimStart;
-    
-
+    private float clipLength;
+    private float activeTime;
     // Start is called before the first frame update
     void Start()
     {
         animRenderer = GetComponent<Renderer>() as SpriteRenderer;
         timeAtAnimStart = Time.timeSinceLevelLoad;
+        clipLength = GetComponent<AudioSource>().clip.length;
     }
 
     // Update is called once per frame
@@ -22,10 +23,13 @@ public class EnemyDeathAnimator : MonoBehaviour
     {
         float timeSinceAnimStart = Time.timeSinceLevelLoad - timeAtAnimStart;
         int frameIndex = (int) (timeSinceAnimStart * framesPerSecond);
-
-        if(frameIndex < deathSprites.Length){
+        if (frameIndex == deathSprites.Length){
+            animRenderer.enabled = false;
+        }
+        else if (frameIndex <= deathSprites.Length){
             animRenderer.sprite = deathSprites[frameIndex];
-        }else{
+        }
+        if (timeSinceAnimStart >= clipLength){
             Destroy(this.gameObject);
         }
     }
